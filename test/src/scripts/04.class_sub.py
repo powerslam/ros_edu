b361e31f1cda657a3f49dac9d53e4153c1338608
powerslam <timsort@kookmin.ac.kr>
2024-01-02 13:28:36 +0900

    spin 함수 위치 수정

--- test/src/scripts/04.class_sub.py
+++ test/src/scripts/04.class_sub.py
@@ -8,8 +8,7 @@ class ClassExample():
     def __init__(self):
         rospy.init_node('wego_sub_node') # 1. node 의 이름 설정(겹치면 한 쪽이 꺼짐)
         rospy.Subscriber('counter', Int32, callback=self.func)
-        rospy.spin() # 2. 대기모드 설정
-
+        
     def func(self, msg: Int32) -> None:
         print(msg.data * 2)
 
@@ -17,7 +16,7 @@ class ClassExample():
 if __name__ == '__main__':
     try:
         obj = ClassExample()
-        obj.func()
+        rospy.spin() # 2. 대기모드 설정
     
     except rospy.ROSInterruptException:
         pass
